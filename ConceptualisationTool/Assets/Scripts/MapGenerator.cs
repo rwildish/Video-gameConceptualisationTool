20bdfbe3c27289f9647104b2047570121211fba8
Ryan Wildish <ryan.wildish@students.plymouth.ac.uk>
2018-02-26 23:15:21 +0000

    Added ColorMap and Regions
    Separated noiseMap generation from MapDsiplay (which now only displays the texture), which is now generated in TextureGenerator, as well as colorMap (noiseMap renamed to heightMap)
    
    colorMap works in the same way as the heightMap does, by using the height (darkness of each pixel to determine height) of each pixel and assigning color to each element in an array. A struct has been created to allow users to create their own colours for the environment they want, with an example already filled in.
    
    The TextureGenerator also makes use of a point filter mode and clamp texture wrap mode
    
    An enum has been created to allow users to swap between the heightMap and colorMap whilst in the editor, so that they can see what the terrain will look like before they press play

--- ConceptualisationTool/Assets/Scripts/MapGenerator.cs
+++ ConceptualisationTool/Assets/Scripts/MapGenerator.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
 
+    public enum DrawMode {NoiseMap, ColorMap};
+    public DrawMode drawMode;
+
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
@@ -18,13 +21,34 @@ public class MapGenerator : MonoBehaviour {
 
     public bool autoUpdate;
 
+    public TerrainType[] regions;
+
     public void GenerateMap()
     {
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistence, lacunarity, offset);
 
-        MapDisplay display = FindObjectOfType<MapDisplay>();
+        Color[] colorMap = new Color[mapWidth * mapHeight];
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for (int i = 0; i < regions.Length; i++)
+                {
+                    if(currentHeight <= regions[i].height)
+                    {
+                        colorMap[y * mapWidth + x] = regions[i].color;
+                        break;
+                    }
+                }
+            }
+        }
 
-        display.DrawNoiseMap(noiseMap);
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        if (drawMode == DrawMode.NoiseMap)
+            display.DrawTexture(TextureGenerator.TextureHeightMap(noiseMap));
+        else if (drawMode == DrawMode.ColorMap)
+            display.DrawTexture(TextureGenerator.TextureColorMap(colorMap, mapWidth, mapHeight));
     }
 
     void OnValidate()
@@ -41,4 +65,12 @@ public class MapGenerator : MonoBehaviour {
         if (octaves < 0)
             octaves = 0;
     }
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color color;
 }
