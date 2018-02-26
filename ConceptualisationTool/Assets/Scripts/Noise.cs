7ad62a013e31f3a8097c2d31d7d0a3518bb7f5a7
Ryan Wildish <ryan.wildish@students.plymouth.ac.uk>
2018-02-26 19:25:08 +0000

    Octaves, Persistence and Lacunarity
    Added in octaves, persistence and lacunarity to affect the noise generated.
    
    Octaves determine the amount of individual layers of noise, Persistence allows for the control of decrease in amplitude and Lacunarity allows for the control of increase in frequency.
    
    Added seed and offset to allow for users to get the same result when using the same seed and for the user to offset on the x and y co-ords to get different noise but at the same level of detail.
    
    Added OnValidate method to allow Unity to control where certain variables values can go, so that the program doesn't crash or have an unintended effect.
    
    Half of width and height variables made so that noiseScale scales from the centre rather than from the top right.

--- ConceptualisationTool/Assets/Scripts/Noise.cs
+++ ConceptualisationTool/Assets/Scripts/Noise.cs
@@ -3,24 +3,71 @@ using UnityEngine;
 
 public static class Noise{
 
-	public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale)
+	public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistence, float lacunarity, Vector2 offset)
     {
         float[,] noiseMap = new float[mapWidth, mapHeight];
+
+        System.Random prng = new System.Random(seed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        for(int i = 0; i < octaves; i++)
+        {
+            float offsetX = prng.Next(-99999, 99999) + offset.x;
+            float offsetY = prng.Next(-99999, 99999) + offset.y;
+            octaveOffsets[i] = new Vector2(offsetX, offsetY);
+        }
 
         if (scale <= 0)
             scale = 0.0001f;
 
-        for(int y = 0; y < mapHeight; y++)
-        {
-            for(int x = 0; x < mapWidth; x++)
-            {
-                float sampleX = x / scale;
-                float sampleY = y / scale;
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
 
-                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
-                noiseMap[x, y] = perlinValue;
+        float halfWidth = mapWidth / 2f;
+        float halfHeight = mapHeight / 2f;
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
+
+                for (int i = 0; i < octaves; i++)
+                {
+
+                    float sampleX = (x - halfWidth) / scale * frequency + octaveOffsets[i].x;
+                    float sampleY = (y - halfHeight) / scale * frequency + octaveOffsets[i].y;
+
+                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+                    noiseHeight += perlinValue * amplitude;
+
+                    amplitude*= persistence;
+                    frequency *= lacunarity;
+                }
+                if(noiseHeight > maxNoiseHeight)
+                {
+                    maxNoiseHeight = noiseHeight;
+                } else if(noiseHeight < minNoiseHeight)
+                {
+                    minNoiseHeight = noiseHeight;
+                }
+
+                noiseMap[x, y] = noiseHeight;
             }
         }
-        return noiseMap;
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                //InverseLerp returns a range from 0-1 so if it returns the value of minNoiseHeight (which can be as low as -1) it will return 0,
+                //wheras if it returns the value of maxNoiseHeight which can be up to 1 (but lets say it is 2 in this example) 
+                //it would return 1
+                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
+            }
+        }
+                return noiseMap;
     }
 }
