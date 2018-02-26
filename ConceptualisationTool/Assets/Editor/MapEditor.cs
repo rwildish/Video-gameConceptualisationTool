b7ae0f6f5dab4bab2a2c7a4584222d1f4846d79b
Ryan Wildish <ryan.wildish@students.plymouth.ac.uk>
2018-02-26 17:43:25 +0000

    Generating Noise map
    Tidied up directory
    
    Created scripts to deal with generating a noise map, displaying the noise map, and editing options of the noise map and other

---
+++ ConceptualisationTool/Assets/Editor/MapEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (MapGenerator))]
+public class MapEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGen = (MapGenerator)target;
+
+        if (DrawDefaultInspector())
+        {
+            if (mapGen.autoUpdate){
+                mapGen.GenerateMap();
+            }
+        }
+
+        if(GUILayout.Button("Generate"))
+        {
+            mapGen.GenerateMap();
+        }
+    }
+}
